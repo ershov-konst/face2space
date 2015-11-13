08d1ccb7769e17e8f4149b8e8d9d038af2d392fa
Mikhail Dudin <dudin-m@live.ru>
2015-11-13 21:30:25 +0300

    Scaffolding of the Game module added

---
+++ modules/Game.js
@@ -0,0 +1,16 @@
+define(['three', 'EventBus'], function(Three, EventBus){
+
+    /**
+     * The class that draws the scene and performs all basic operations of the game
+     * @param $placeholder DOM element where the scene will be rendered
+     */
+    function Game($placeholder) {
+        //TODO: build a scene and set up a camera here
+    }
+
+    Game.prototype.start = function() {
+        // TODO: start animation
+    }
+
+    return Game;
+});
