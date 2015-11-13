f63cab0c9e5dfec7a04f43f2fb4df5c57f33654f
Mikhail Dudin <dudin-m@live.ru>
2015-11-14 00:05:08 +0300

    comment fixed

--- modules/Game.js
+++ modules/Game.js
@@ -7,7 +7,7 @@ define(['three', 'EventBus'], function(Three, EventBus){
 
     /**
      * The class that draws the scene and performs all basic operations of the game
-     * @param $placeholder DOM element where the scene will be rendered
+     * @param $placeholder jQuery wrapped DOM element where the scene will be rendered
      */
     function Game($placeholder) {
         scene = new Three.Scene();
