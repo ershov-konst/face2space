c3f7a0c1e963a4e6ff2b0fb0c9723c97e604a179
takentui <takentui@gmail.com>
2015-11-13 23:44:22 +0300

    Добавил класс Checker и метод checkObject

---
+++ modules/checker.js
@@ -0,0 +1,16 @@
+define(function(){
+    function Checker(Camera) {
+        this.camera = Camera;
+
+    }
+
+    Checker.prototype.checkObject = function (sceneObject) {
+            if ( sceneObject.positionZ < this.camera.positionZ )
+                return -1; //недолет
+            else if ( sceneObject.positionX <= ( this.camera.positionX + window.innerWidth / 2 ) && sceneObject.positionX >= ( this.camera.positionX - window.innerWidth / 2 ) && sceneObject.positionY <= ( this.camera.positionY + window.innerHeight / 2 ) && sceneObject.positionY >= ( this.camera.positionY - window.innerHeight / 2 ))
+                    return 0; // попадание
+                else
+                    return 1; // мимо
+    }
+    return Checker;
+});
