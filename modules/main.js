8594ad9edb1304876abda7e5109edaee66114cb9
takentui <takentui@gmail.com>
2015-11-13 23:47:06 +0300

    Добавил пример использования моих кусков

--- modules/main.js
+++ modules/main.js
@@ -11,5 +11,20 @@ require(['three', 'EventBus'], function(tm, THREE, EventBus){
    console.log(t.testMethod(2));
    console.log(THREE);
    console.log(EventBus);
-
+    //пример использования
+   // var scene = {
+   //     minX: 0,
+   //     maxX: window.innerWidth + 1000,
+   //     minY: 0,
+   //     maxY: window.innerHeight + 1000
+   // };
+   // var check = new Checker({positionX: window.innerWidth / 2, positionY: window.innerHeight / 2, positionZ: 0});
+   // var t = new Generator(scene);
+   // var k;
+   // for ( var i = 0; i < 100 ; i ++)
+   // {
+   //     k = t.getObject();
+   //     console.log(k);
+   //     console.log(check.checkObject(k));
+   // }
 });
