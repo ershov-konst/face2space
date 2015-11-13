d00c61db76e798f6ceb1b190d77a9de5598866ac
ryabovnikita <ckujime@gmail.com>
2015-11-13 23:09:46 +0300

    master change

--- modules/Master.js
+++ modules/Master.js
@@ -1,4 +1,22 @@
 define(['jquery'], function($){
 
-   var r = $('#root');
+    var r = $('#root');
+    var g = new Game(r);
+
+    $(document).ready(function(){
+        $(document).on('keydown', function(e){
+            if(e.keyCode === 32) {
+                g.start();
+
+                if(g.lose()) {
+                    $(location).attr('href', "3экран")
+                }
+                //changeLives
+                setInterval(function(){
+                    g.getScore();
+                    g.redraw();
+                },20);
+            }
+        });
+    });
 });
