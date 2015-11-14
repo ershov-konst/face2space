b657e90dca429f64f1033a37507b592b9b395297
Mikhail Dudin <dudin-m@live.ru>
2015-11-14 09:38:18 +0300

    Added headtrackr.js

--- modules/main.js
+++ modules/main.js
@@ -2,7 +2,8 @@ requirejs.config({
    paths: {
       'three': '../lib/three',
       'EventBus': '../lib/EventBus',
-      'jquery': '../lib/jquery-2.1.4.min'
+      'jquery': '../lib/jquery-2.1.4.min',
+      'HeadTracker': '../lib/headtrackr'
    }
 });
 
