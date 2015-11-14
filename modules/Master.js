bf7a785c8f40966e47e657dd1a49e387a58732db
ershov-konst <ershov.konst@gmail.com>
2015-11-14 13:47:21 +0300

    integration step 1

--- modules/Master.js
+++ modules/Master.js
@@ -1,32 +1,33 @@
-define(['jquery'/*, 'Game'*/], function ($, Game) {
+define(['jquery', 'Game'], function ($, Game) {
     //TODO тут все норм, пока это не трогаем
 
     var
-        STATUS_STOPED  = 0,
+        STATUS_STOPED = 0,
         STATUS_STARTED = 1,
-        STATUS_PAUSED  = 2;
+        STATUS_PAUSED = 2;
 
     var
         gameStatus = STATUS_STOPED,
         forRender = $('#render'),
         mainDisplay = $('.main-display'),
         gameDisplay = $('.game-display'),
-        lives       = gameDisplay.find('.lives'),
-        score       = gameDisplay.find('.score'),
-        achievem    = gameDisplay.find('.achievemgame');
+        lives = gameDisplay.find('.lives'),
+        score = gameDisplay.find('.score'),
+        achievem = gameDisplay.find('.achievemgame'),
+        livesCount = 0;
 
-    function changeLives(i){
+    function changeLives(i) {
         lives.empty();
-        for(var j=0; j < i; j++){
+        for (var j = 0; j < i; j++) {
             lives.append('<div class="live"></div>');
         }
     }
 
-    function changeScore(score){
+    function changeScore(score) {
         score.html(score);
     }
 
-    function showAchiev(ach){
+    function showAchiev(ach) {
         achievem.html(ach);
         achievem.fadeIn('slow');
         setTimeout(achievem.fadeOut(2000), 5000);
@@ -37,20 +38,22 @@ define(['jquery'/*, 'Game'*/], function ($, Game) {
 
             //Обработка пробела
 
-            if (STATUS_STOPED){
+            if (STATUS_STOPED) {
 
                 var g = new Game(forRender);
+                livesCount = 3;
+                changeLives(livesCount);
+                mainDisplay.hide();
+                gameDisplay.show();
+
                 g.start();
-                g.on('lose', function(){
-                    //показываем финальный экран
-                });
-                g.on('changeLives', function(lives){
+                g.on('changeLives', function (lives) {
                     changeLives(lives);
                 });
 
-                /*setInterval(function () {
+                setInterval(function () {
                     changeScore(g.getScore());
-                }, 1000/10);*/
+                }, 1000 / 10);
 
             }
         }
