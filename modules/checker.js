1067161e2972f234aa7ec7fe7bc724d3069b45ad
ershov-konst <ershov.konst@gmail.com>
2015-11-14 21:08:35 +0300

    подгоняем hitbox

--- modules/checker.js
+++ modules/checker.js
@@ -1,28 +1,23 @@
 define(function(){
     function Checker(Camera) {
         this.camera = Camera;
-        var vFOV = this.camera.fov * Math.PI / 180;
-        console.log(this.camera.fov);
-        console.log(vFOV);
         var aspect = window.innerWidth / window.innerHeight;
-        this.halfHitBoxHeight = 1;
+        this.halfHitBoxHeight = 9;
         this.halfHitBoxWidth  = (this.halfHitBoxHeight * aspect)/2;
     }
 
     Checker.prototype.checkObject = function (sceneObject) {
-        console.log(this.camera.position.x, this.camera.position.y);
-        if ( sceneObject.position.z < this.camera.position.z - 10 )
+
+        if ( sceneObject.position.z < this.camera.position.z - 20 )
             return Checker.FAR; //недолет
         else if (
-            (
-                sceneObject.position.x >= this.camera.position.x - this.halfHitBoxWidth &&
-                sceneObject.position.x <= this.camera.position.x + this.halfHitBoxWidth
-            )
-            ||
-            (
-                sceneObject.position.y >= this.camera.position.y - this.halfHitBoxHeight &&
-                sceneObject.position.y <= this.camera.position.y + this.halfHitBoxHeight
-            )
+
+            sceneObject.position.x >= this.camera.position.x - this.halfHitBoxWidth &&
+            sceneObject.position.x <= this.camera.position.x + this.halfHitBoxWidth &&
+
+            sceneObject.position.y >= this.camera.position.y - this.halfHitBoxHeight &&
+            sceneObject.position.y <= this.camera.position.y + this.halfHitBoxHeight
+
         ){
             if (sceneObject.isBonus) {
                 return Checker.HIT_BONUS; // hit bonus
