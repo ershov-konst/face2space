512dd766b8c3b0b072d070896a9e6ca85b24d95a
takentui <takentui@gmail.com>
2015-11-14 15:17:34 +0300

    Управление головой

--- modules/Master.js
+++ modules/Master.js
@@ -1,5 +1,24 @@
-define(['jquery', 'Game'], function($, Game){
+define(['jquery', 'Game','HeadTracker'], function($, Game, HeadTracker){
     var r = $('#root');
     var g = new Game($('#placeholder'));
-    g.start();
+    var videoInput = document.getElementById('inputVideo');
+    var canvasInput = document.getElementById('inputCanvas');
+
+    var htracker = new HeadTracker.Tracker();
+    console.log(htracker);
+    htracker.init(videoInput, canvasInput);
+    htracker.start();
+    $(document).on('headtrackingEvent', function(e,k) {
+        var x = e.originalEvent.x;
+        var y = e.originalEvent.y;
+        var z = e.originalEvent.z;
+        console.log(x + " = " + y + " - " + z);
+        g.headMoved(x, y);
+        // console.log(x + " - " + y);
+    });
+    $(document).on('headtrackrStatus', function (e) {
+        var status = e.originalEvent.status;
+    })
+
+    //g.start();
 });
